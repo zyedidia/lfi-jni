b6c17db30945268c62a14473e227153e9438112e
Zachary Yedidia <zyedidia@gmail.com>
2025-03-21 13:52:31 -0700

    Minor update

--- hello/Hello.c
+++ hello/Hello.c
@@ -5,5 +5,5 @@ JNIEXPORT void JNICALL
 Java_Hello_sayHello(JNIEnv* env, jobject thisObject)
 {
     jint version = (*env)->GetVersion(env);
-    printf("version: %d\n", version);
+    printf("Hello! JDK version: %d\n", version);
 }
